3093cbea19c879cabb351aa8cdbc6f552599ebc9
Kenneth Nelson <kenneth.nelson.dev@gmail.com>
2025-10-10 10:59:32 +0000

    feat: Enhance classnames helper for better DX

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+export type { ClassValue } from 'clsx';
+
+/**
+ * A utility function for conditionally joining CSS class names together.
+ * It uses `clsx` to handle various input types and `tailwind-merge` to resolve
+ * Tailwind CSS conflicts by merging conflicting classes.
+ *
+ * @param inputs - An array of class values, which can be strings, objects,
+ *                 arrays, or `undefined`/`null` values. Falsy values are ignored.
+ * @returns A single string containing the merged and de-conflicted CSS class names.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
---
+++ temp_test.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var utils_ts_1 = require("./src/lib/utils.ts");
+console.log("Manual check for cn('a', undefined, ['b']):", (0, utils_ts_1.cn)('a', undefined, ['b']));
